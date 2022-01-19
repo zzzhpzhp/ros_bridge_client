1d4df332c141f846c63952c009e97f3a6570a0a9
zzzhpzhp <917309156@qq.com>
2022-01-19 12:26:13 +0800

    添加线条、线段和多边形数据发布API。

--- ros_bridge_client.h
+++ ros_bridge_client.h
@@ -151,11 +151,21 @@ namespace ros_bridge_client
             msg["color"]["b"] = b;
             msg["color"]["a"] = a;
 
+            // 偏移值
+            msg["pose"]["position"]["x"] = 0.0f;
+            msg["pose"]["position"]["y"] = 0.0f;
+            msg["pose"]["position"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["x"] = 0.0f;
+            msg["pose"]["orientation"]["y"] = 0.0f;
+            msg["pose"]["orientation"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["w"] = 1.0f;
+
             // 尺寸不得小于等于0
             size_x = std::max(0.01f, size_x);
             size_y = std::max(0.01f, size_y);
             msg["scale"]["x"] = size_x;
             msg["scale"]["y"] = size_y;
+            msg["scale"]["z"] = 1.0f;
 
             Json::Value point;
             for (const auto &p : points)
@@ -164,6 +174,114 @@ namespace ros_bridge_client
                 point["y"] = p.y;
                 point["z"] = 0.0f;
 
+                msg["points"].append(point);
+            }
+
+            _publish_msg(msg);
+        }
+
+        template<typename T>
+        void publishLineStrip(std::string topic_name, std::string frame_id, const T &points, float size_x, float r, float g, float b, float a = 1.0)
+        {
+            Json::Value msg;
+            msg["topic_name"] = topic_name;
+            msg["type"] = "visualization_msgs::Marker";
+            msg["header"]["frame_id"] = frame_id;
+            msg["ns"] = "line_strip";
+            msg["id"] = 0;
+            msg["action"] = ADD;
+            msg["marker_type"] = LINE_STRIP;
+            msg["color"]["r"] = r;
+            msg["color"]["g"] = g;
+            msg["color"]["b"] = b;
+            msg["color"]["a"] = a;
+
+            // 偏移值
+            msg["pose"]["position"]["x"] = 0.0f;
+            msg["pose"]["position"]["y"] = 0.0f;
+            msg["pose"]["position"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["x"] = 0.0f;
+            msg["pose"]["orientation"]["y"] = 0.0f;
+            msg["pose"]["orientation"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["w"] = 1.0f;
+
+            // 尺寸不得小于等于0
+            size_x = std::max(0.01f, size_x);
+            msg["scale"]["x"] = size_x;
+
+            Json::Value point;
+            for (const auto &p : points)
+            {
+                point["x"] = p.x;
+                point["y"] = p.y;
+                point["z"] = 0.0f;
+
+                msg["points"].append(point);
+            }
+
+            _publish_msg(msg);
+        }
+
+        template<typename T>
+        void publishLineList(std::string topic_name, std::string frame_id, const T &points, float size_x, float r, float g, float b, float a = 1.0)
+        {
+            if (points.size() %2 != 0)
+            {
+                std::cerr << "Line list points size should be odd." << std::endl;
+            }
+            Json::Value msg;
+            msg["topic_name"] = topic_name;
+            msg["type"] = "visualization_msgs::Marker";
+            msg["header"]["frame_id"] = frame_id;
+            msg["ns"] = "line_list";
+            msg["id"] = 0;
+            msg["action"] = ADD;
+            msg["marker_type"] = LINE_LIST;
+            msg["color"]["r"] = r;
+            msg["color"]["g"] = g;
+            msg["color"]["b"] = b;
+            msg["color"]["a"] = a;
+
+            // 偏移值
+            msg["pose"]["position"]["x"] = 0.0f;
+            msg["pose"]["position"]["y"] = 0.0f;
+            msg["pose"]["position"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["x"] = 0.0f;
+            msg["pose"]["orientation"]["y"] = 0.0f;
+            msg["pose"]["orientation"]["z"] = 0.0f;
+            msg["pose"]["orientation"]["w"] = 1.0f;
+
+            // 尺寸不得小于等于0
+            size_x = std::max(0.01f, size_x);
+            msg["scale"]["x"] = size_x;
+
+            Json::Value point;
+            for (const auto &p : points)
+            {
+                point["x"] = p.x;
+                point["y"] = p.y;
+                point["z"] = 0.0f;
+
+                msg["points"].append(point);
+            }
+
+            _publish_msg(msg);
+        }
+
+
+        template<typename T>
+        void publishPolygon(std::string topic_name, std::string frame_id, const T &points)
+        {
+            Json::Value msg;
+            msg["topic_name"] = topic_name;
+            msg["type"] = "geometry_msgs::PolygonStamped";
+            msg["header"]["frame_id"] = frame_id;
+
+            Json::Value point;
+            for (const auto &p : points)
+            {
+                point["x"] = p.x;
+                point["y"] = p.y;
                 msg["points"].append(point);
             }
 
